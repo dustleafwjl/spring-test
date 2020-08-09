dd0f05580fd1fc0886c552fce52dba33b20a37f5
dustleafwjl <wangjianglin_priv@qq.com>
2020-08-09 23:01:09 +0800

    [jianglin wang] feat: should update re event when buy rs event has exist in trade only

--- src/test/java/com/thoughtworks/rslist/service/RsServiceTest.java
+++ src/test/java/com/thoughtworks/rslist/service/RsServiceTest.java
@@ -2,10 +2,7 @@ package com.thoughtworks.rslist.service;
 
 import com.thoughtworks.rslist.domain.Trade;
 import com.thoughtworks.rslist.domain.Vote;
-import com.thoughtworks.rslist.dto.RsEventDto;
-import com.thoughtworks.rslist.dto.TradeOnlyDto;
-import com.thoughtworks.rslist.dto.UserDto;
-import com.thoughtworks.rslist.dto.VoteDto;
+import com.thoughtworks.rslist.dto.*;
 import com.thoughtworks.rslist.exception.AmountIsLessException;
 import com.thoughtworks.rslist.repository.*;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +13,7 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -31,6 +29,8 @@ class RsServiceTest {
   TradeOnlyRepository tradeOnlyRepository;
   @Mock
   TradeRepository tradeRepository;
+
+
   LocalDateTime localDateTime;
   Vote vote;
 
@@ -118,7 +118,14 @@ class RsServiceTest {
                     .user(userDto)
                     .build();
 
+    TradeOnlyDto tradeOnlyDto =
+            TradeOnlyDto.builder()
+                    .rsEvent(rsEventDto)
+                    .rank(1)
+                    .amount(24)
+                    .build();
     when(rsEventRepository.findById(anyInt())).thenReturn(Optional.of(rsEventDto));
+    when(tradeOnlyRepository.save(any())).thenReturn(tradeOnlyDto);
     rsService.buy(Trade.builder().amount(24).rank(1).build(), 2);
     // then
     verify(tradeOnlyRepository)
@@ -162,8 +169,10 @@ class RsServiceTest {
                     .build();
 
 
+
     when(rsEventRepository.findById(anyInt())).thenReturn(Optional.of(rsEventDtoBuySecond));
     when(tradeOnlyRepository.findByRank(anyInt())).thenReturn(Optional.of(tradeOnlyDto));
+    when(tradeOnlyRepository.save(any())).thenReturn(tradeOnlyDto);
     // when
     rsService.buy(Trade.builder().amount(26).rank(1).build(), 2);
 //     then
@@ -218,5 +227,54 @@ class RsServiceTest {
             () -> {
               rsService.buy(Trade.builder().amount(22).rank(1).build(), 2);
             });
+  }
+  @Test
+  void shouldUpdateRsEventWhenBuyGivenRsEventHasExistInTradeOnly() {
+    // given
+    UserDto userDto =
+            UserDto.builder()
+                    .voteNum(5)
+                    .phone("18888888888")
+                    .gender("female")
+                    .email("a@b.com")
+                    .age(19)
+                    .userName("xiaoli")
+                    .id(1)
+                    .build();
+    RsEventDto rsEventDtoBuyFirst =
+            RsEventDto.builder()
+                    .eventName("event name")
+                    .id(2)
+                    .keyword("keyword")
+                    .voteNum(2)
+                    .user(userDto)
+                    .build();
+
+    TradeOnlyDto tradeOnlyDto =
+            TradeOnlyDto.builder()
+                    .rsEvent(rsEventDtoBuyFirst)
+                    .rank(1)
+                    .amount(24)
+                    .build();
+
+    TradeOnlyDto tradeOnlyDtoSave =
+            TradeOnlyDto.builder()
+                    .rsEvent(rsEventDtoBuyFirst)
+                    .rank(2)
+                    .amount(26)
+                    .build();
+
+
+    when(rsEventRepository.findById(anyInt())).thenReturn(Optional.of(rsEventDtoBuyFirst));
+    when(tradeOnlyRepository.findByRank(anyInt())).thenReturn(Optional.of(tradeOnlyDto));
+    when(tradeOnlyRepository.findByRsEventId(anyInt())).thenReturn(Optional.of(tradeOnlyDto));
+    when(tradeOnlyRepository.save(any())).thenReturn(tradeOnlyDtoSave);
+
+    // when
+    rsService.buy(Trade.builder().amount(26).rank(2).build(), 2);
+    // then
+    verify(tradeRepository)
+            .save(
+                    TradeDto.builder().amount(26).rank(2).rsEvent(rsEventDtoBuyFirst).build());
   }
 }
